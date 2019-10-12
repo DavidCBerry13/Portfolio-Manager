0f2ee666262838b57b84dca7c3a208efef10bf12
David Berry <dberry83702@yahoo.com>
2019-10-12 15:47:55 -0500

    Added the columns for AccountMarketValues as they were missing

--- database/database-projects/PortfolioManagerDatabase/PortfolioManagerDatabase/Tables/AccountMarketValues.sql
+++ database/database-projects/PortfolioManagerDatabase/PortfolioManagerDatabase/Tables/AccountMarketValues.sql
@@ -1,4 +1,12 @@
 ﻿CREATE TABLE [dbo].[AccountMarketValues]
 (
-	[Id] INT NOT NULL PRIMARY KEY
-)
+	TradeDate       DATE          NOT NULL,
+	AccountNumber   VARCHAR(12)   NOT NULL,
+	MarketValue     DECIMAL(14,4) NOT NULL,
+	CONSTRAINT PK_AccountMarketValues
+	    PRIMARY KEY (TradeDate, AccountNumber),
+	CONSTRAINT FK_AccountMarketValues_TradeDate
+	    FOREIGN KEY (TradeDate) REFERENCES TradeDates (TradeDate),
+	CONSTRAINT FK_AccountMarketValues_AccountNumber
+	    FOREIGN KEY (AccountNumber) REFERENCES InvestmentAccounts (AccountNumber)
+);
