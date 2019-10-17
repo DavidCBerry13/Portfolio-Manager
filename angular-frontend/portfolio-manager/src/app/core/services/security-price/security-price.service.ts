9bffbca300d89fdcbc2ef8e965225624e484611f
David Berry <dberry83702@yahoo.com>
2019-10-17 00:16:54 -0500

    Implemented a BaseService class to factor out common functionality around error handling and logging

--- angular-frontend/portfolio-manager/src/app/core/services/security-price/security-price.service.ts
+++ angular-frontend/portfolio-manager/src/app/core/services/security-price/security-price.service.ts
@@ -4,20 +4,18 @@ import { SecurityPrice } from './security-price';
 import { Observable, of } from 'rxjs';
 import { catchError, map, tap } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
+import { BaseService } from '../base.service';
 
 
 @Injectable({
   providedIn: 'root'
 })
-export class SecurityPriceService {
+export class SecurityPriceService extends BaseService {
 
   private PRICES_ENDPOINT = '/api/Prices';
 
 
-  constructor(private http: HttpClient) { }
-
-
-  getSecurities (): Observable<SecurityPrice[]> {
+  getSecurities(): Observable<SecurityPrice[]> {
     return this.http.get<SecurityPrice[]>(environment.stockDataUrl + this.PRICES_ENDPOINT)
       .pipe(
         tap(_ => this.log('fetched securities')),
@@ -25,32 +23,5 @@ export class SecurityPriceService {
       );
   }
 
-
-
-
-  /**
-   * Handle Http operation that failed.
-   * Let the app continue.
-   * @param operation - name of the operation that failed
-   * @param result - optional value to return as the observable result
-   */
-  private handleError<T> (operation = 'operation', result?: T) {
-    return (error: any): Observable<T> => {
-
-      // TODO: send the error to remote logging infrastructure
-      console.error(error); // log to console instead
-
-      // TODO: better job of transforming error for user consumption
-      this.log(`${operation} failed: ${error.message}`);
-
-      // Let the app keep running by returning an empty result.
-      return of(result as T);
-    };
-  }
-
-  /** Log a HeroService message with the MessageService */
-  private log(message: string) {
-    //this.messageService.add(`HeroService: ${message}`);
-  }
 
 }
