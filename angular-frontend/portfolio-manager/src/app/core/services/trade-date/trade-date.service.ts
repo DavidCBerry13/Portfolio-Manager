878ccc0ca0f9a5d776eee9b43dd021f9f5bb1c09
David Berry <dberry83702@yahoo.com>
2019-10-17 20:43:36 -0500

    Trade Date Service inheriting from common base class

--- angular-frontend/portfolio-manager/src/app/core/services/trade-date/trade-date.service.ts
+++ angular-frontend/portfolio-manager/src/app/core/services/trade-date/trade-date.service.ts
@@ -4,16 +4,15 @@ import { Observable, of } from 'rxjs';
 import { catchError, map, tap } from 'rxjs/operators';
 import { TradeDate } from './trade-date';
 import { environment } from 'src/environments/environment';
+import { BaseService } from '../base.service';
 
 
 @Injectable({
   providedIn: 'root'
 })
-export class TradeDateService {
+export class TradeDateService extends BaseService {
 
   private TRADE_DATES_ENDPOINT = '/api/TradeDates';
-
-  constructor(private http: HttpClient) { }
 
   getTradeDates(): Observable<TradeDate[]> {
     return this.http.get<TradeDate[]>(environment.stockDataUrl + this.TRADE_DATES_ENDPOINT)
@@ -22,32 +21,6 @@ export class TradeDateService {
         catchError(this.handleError<TradeDate[]>('getTradeDates', []))
       );
   }
-
-  /**
-   * Handle Http operation that failed.
-   * Let the app continue.
-   * @param operation - name of the operation that failed
-   * @param result - optional value to return as the observable result
-   */
-  private handleError<T> (operation = 'operation', result?: T) {
-    return (error: any): Observable<T> => {
-
-      // TODO: send the error to remote logging infrastructure
-      console.error(error); // log to console instead
-
-      // TODO: better job of transforming error for user consumption
-      this.log(`${operation} failed: ${error.message}`);
-
-      // Let the app keep running by returning an empty result.
-      return of(result as T);
-    };
-  }
-
-  /** Log a HeroService message with the MessageService */
-  private log(message: string) {
-    //this.messageService.add(`HeroService: ${message}`);
-  }
-
 
 
 }
