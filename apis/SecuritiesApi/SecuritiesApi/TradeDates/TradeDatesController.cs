732988afb27eaa87124b60794c8f1d61877aff76
David Berry <dberry83702@yahoo.com>
2020-01-17 21:42:31 -0600

    Made sure controller returns correct HTTP status code based on if error is received from the service layer.  Added attributes and comments for Swagger documentation.

--- apis/SecuritiesApi/SecuritiesApi/TradeDates/TradeDatesController.cs
+++ apis/SecuritiesApi/SecuritiesApi/TradeDates/TradeDatesController.cs
@@ -4,12 +4,15 @@ using System.Linq;
 using System.Threading.Tasks;
 using AutoMapper;
 using DavidBerry.Framework.ApiUtil.Controllers;
+
 using DavidBerry.Framework.ApiUtil.Models;
+using DavidBerry.Framework.Functional;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Securities.Core.AppInterfaces;
 using Securities.Core.Domain;
+using Securities.Core.Errors;
 
 namespace SecuritiesApi.TradeDates
 {
@@ -33,7 +36,11 @@ namespace SecuritiesApi.TradeDates
         /// Gets a list of all trade dates in the system, that is, dates where US Securities were traded and we have data for
         /// </summary>
         /// <returns>A List of TradeDateModel objects</returns>
+        /// <response code="200">Success.  A list of TradeDateModel objects representing all available trade dates will be returned</response>
+        /// <response code="500">Internal Server Error</response>
         [HttpGet(Name = "GetTradeDates")]
+        [ProducesResponseType(typeof(TradeDateModel), 200)]
+        [ProducesResponseType(typeof(ApiMessageModel), 500)]
         public IActionResult Get()
         {
             var result = _tradeDateService.GetTradeDates();
@@ -45,13 +52,23 @@ namespace SecuritiesApi.TradeDates
             }
             else
             {
-                return ControllerExtensions.InternalServerError(this, new ApiMessageModel() { Message = result.Error.Message });
+                return HandleErrorResult(result);
             }
         }
 
 
-
+        /// <summary>
+        /// Gets the trade date information associated with the given date.  This method can also be used to check if a date is a valid trade date
+        /// </summary>
+        /// <param name="date">A Date value</param>
+        /// <returns></returns>
+        /// <response code="200">Success.  The corresponding TradeDateModel object will be returned</response>
+        /// <response code="400">Bad Request.  No corresponding trade date can be found for the provided date.  Most likely this means the provided date is not a valid trade date (for example, a weekend or holiday)</response>
+        /// <response code="500">Internal Server Error</response>
         [HttpGet("{date:datetime}", Name = "GetTradeDate")]
+        [ProducesResponseType(typeof(TradeDateModel), 200)]
+        [ProducesResponseType(typeof(ApiMessageModel), 400)]
+        [ProducesResponseType(typeof(ApiMessageModel), 500)]
         public IActionResult Get(DateTime date)
         {
             var result = _tradeDateService.GetTradeDate(date.Date);
@@ -62,13 +79,20 @@ namespace SecuritiesApi.TradeDates
             }
             else
             {
-                return BadRequest(new ApiMessageModel() { Message = result.Error.Message });
+                return HandleErrorResult(result);
             }
         }
 
 
-
+        /// <summary>
+        /// Gets the latest (most current) trade date loaded in the system
+        /// </summary>
+        /// <returns></returns>
+        /// <response code="200">Success.  A TradeDateModel representing the most recent trade date is returned</response>
+        /// <response code="500">Internal application error.  An error occurred on the server</response>
         [HttpGet("Latest", Name = "GetLatestTradeDate")]
+        [ProducesResponseType(typeof(TradeDateModel), 200)]
+        [ProducesResponseType(typeof(ApiMessageModel), 500)]
         public IActionResult GetLatestTradeDate()
         {
             var result = _tradeDateService.GetLatestTradeDate();
@@ -80,7 +104,7 @@ namespace SecuritiesApi.TradeDates
             }
             else
             {
-                return ControllerExtensions.InternalServerError(this, new ApiMessageModel() { Message = result.Error.Message });
+                return HandleErrorResult(result);
             }
 
         }
@@ -88,5 +112,39 @@ namespace SecuritiesApi.TradeDates
 
 
 
+
+        protected override void LogErrorResult(Result result)
+        {
+            switch (result.Error)
+            {
+                case InvalidTradeDateError error:
+                    _logger.LogInformation(result.Error.Message);
+                    break;
+                case ApplicationError error:
+                    _logger.LogError(result.Error.Message);
+                    break;
+                default:
+                    _logger.LogError(result.Error.Message);
+                    break;
+            }
+        }
+
+
+
+        protected override ActionResult MapErrorResult(Result result)
+        {
+            switch (result.Error)
+            {
+                case InvalidTradeDateError error:
+                    return BadRequest(new ApiMessageModel() { Message = result.Error.Message });
+                case ApplicationError error:
+                    return ControllerExtensions.InternalServerError(this, new ApiMessageModel() { Message = result.Error.Message });
+                default:
+                    return ControllerExtensions.InternalServerError(this, new ApiMessageModel() { Message = result.Error.Message });
+            }
+        }
+
+
+
+        }
     }
-}
